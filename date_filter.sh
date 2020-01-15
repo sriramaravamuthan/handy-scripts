5e0eca303e897858f7a329694a3cf64e8f8abba3
Sriram <57688417+sriramaravamuthan@users.noreply.github.com>
2020-01-15 16:07:02 +1300

    ran shellcheck and corrected the logical expression

--- date_filter.sh
+++ date_filter.sh
@@ -31,25 +31,27 @@ fi
 input_file=$1
 input_start_date=$2
 input_end_date=$3
-fmt_start_date=$(date -d $input_start_date +"%s")
-fmt_end_date=$(date -d $input_end_date +"%s")
+fmt_start_date=$(date -d "$input_start_date" +"%s")
+fmt_end_date=$(date -d "$input_end_date" +"%s")
 output_file=$4
-rm -rf $output_file
-while IFS=, read driverId startDateTime endDateTime
+suffix_start_time="T00:00:00"
+suffix_end_time="T23:59:59"
+rm -rf "$output_file"
+while IFS=, read -r driverId startDateTime endDateTime
 do
-     rec_start_date=$(date -d $startDateTime +"%s")
-     rec_end_date=$(date -d $endDateTime +"%s")
+     rec_start_date=$(date -d "$startDateTime" +"%s")
+     rec_end_date=$(date -d "$endDateTime" +"%s")
      if [[ "$rec_start_date" -ge  "$fmt_start_date" && "$rec_end_date" -le "$fmt_end_date" ]];
      then
-        echo "$driverId,$startDateTime,$endDateTime" >> $output_file
+        echo "$driverId","$startDateTime","$endDateTime" >> "$output_file"
      elif [[ "$rec_start_date" -le  "$fmt_start_date" && "$rec_end_date" -ge "$fmt_end_date" ]];
      then
-        echo "$driverId,"$input_start_date"T00:00:00,"$input_end_date"T23:59:59" >> $output_file
-     elif [[ "$rec_end_date" -le "$fmt_end_date" && "$rec_end_date" -ge "$fmt_start_date" ]] || [[ "$rec_start_date" -ge "$fmt_start_date" && "$rec_end_date" -ge "$fmt_end_date" ]];
+        echo "$driverId","$input_start_date""$suffix_start_time","$input_end_date""$suffix_end_time" >> "$output_file"
+     elif [[ "$rec_end_date" -le "$fmt_end_date" && "$rec_end_date" -ge "$fmt_start_date" ]];
      then
-        echo "$driverId,"$input_start_date"T00:00:00,"$endDateTime"" >> $output_file
-     elif [[ "$rec_start_date" -ge "$fmt_start_date" && "$rec_end_date" -ge "$fmt_end_date" ]];
+        echo "$driverId","$input_start_date""$suffix_start_time","$endDateTime" >> "$output_file"
+     elif [[ "$rec_start_date" -ge "$fmt_start_date" && "$rec_start_date" -le "$fmt_end_date" ]];
      then
-       echo "$driverId,"$startDateTime","$input_end_date"T23:59:59" >> $output_file
+       echo "$driverId","$startDateTime","$input_end_date""$suffix_end_time" >> "$output_file"
      fi
-done < $input_file
+done < "$input_file"
